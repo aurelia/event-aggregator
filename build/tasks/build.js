9afa4dd1f85e7b7869792f284b3f97373234f305
Rob Eisenberg <rob@bluespire.com>
2015-07-29 20:14:21 -0400

    chore(build): improve output file name

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -6,36 +6,39 @@ var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 var rename = require('gulp-rename');
 
+var jsName = paths.packageName + '.js';
+
 gulp.task('build-index', function(){
   return gulp.src(paths.root + 'index.js')
+    .pipe(rename(jsName))
     .pipe(gulp.dest(paths.output));
 });
 
 gulp.task('build-es6', function () {
-  return gulp.src(paths.output + 'index.js')
+  return gulp.src(paths.output + jsName)
     .pipe(gulp.dest(paths.output + 'es6'));
 });
 
 gulp.task('build-commonjs', function () {
-  return gulp.src(paths.output + 'index.js')
+  return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions, {modules:'common'})))
     .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 
 gulp.task('build-amd', function () {
-  return gulp.src(paths.output + 'index.js')
+  return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions, {modules:'amd'})))
     .pipe(gulp.dest(paths.output + 'amd'));
 });
 
 gulp.task('build-system', function () {
-  return gulp.src(paths.output + 'index.js')
+  return gulp.src(paths.output + jsName)
     .pipe(to5(assign({}, compilerOptions, {modules:'system'})))
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
 gulp.task('build-dts', function(){
-  return gulp.src(paths.output + 'index.d.ts')
+  return gulp.src(paths.output + paths.packageName + '.d.ts')
       .pipe(rename(paths.packageName + '.d.ts'))
       .pipe(gulp.dest(paths.output + 'es6'))
       .pipe(gulp.dest(paths.output + 'commonjs'))
